10cda689e18ca26d1f3b322eec6b6190145872e0
GabrielGalucio <gmlgalucio@gmail.com>
2020-04-29 22:02:52 -0400

    002_commit

---
+++ algoritmos_ordenacao/001_selection_sort.py
@@ -0,0 +1,15 @@
+# ORDENAÇÃO POR SELEÇÃO
+
+# Uma primeira idéia de selection sorte é verificar o maior ou menor elemento de uma lista
+# não ordenada e começa a ordenar. DÃ! ou seja, pega o primeiro elemento (maior ou menor) e
+# coloca na ordem, e repete até pegar todos os elementos de uma lista e os colocar em ordem
+
+# Considere uma lista:
+# Os números serão ordenados de ordem crescente sempre substituindo a posição de acordo em
+# que se vai identificando o menor valor possivel, até formarem uma lista ordenada.
+ 
+# lista_versao0 = [7, 8, 3, 4, 1, 5]
+# lista_versao1 = [1, 8, 3, 4, 7, 5]
+# lista_versao2 = [1, 3, 8, 4, 7, 5]
+# lista_versao3 = [1, 3, 4, 8, 7, 5]
+# lista_versao4 = [1, 3, 4, 5, 7, 8]
